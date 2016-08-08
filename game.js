dd267eca63e78b8633b7d06fe4d4a1479508ec3b
rowieej <rowena.jaico@gmail.com>
2016-08-08 15:00:06 -0400

    updating files

--- game.js
+++ game.js
@@ -1,11 +1,13 @@
 //randomly select a word for the player
+// var selectFood = ["pizza", "sandwich", "taco", "burrito", "hamburger", "spaghetti"];
 
+// var randomFood = function (){
+//   var guessFood = selectFood[Math.floor(Math.random()* 6)];
+//   console.log(guessFood);
+// }
 
-var selectFood = ["pizza", "sandwich", "taco", "burrito", "hamburger", "spaghetti"];
+// randomFood();
 
-var randomFood = function (){
-  var guessFood = selectFood[Math.floor(Math.random()* 6)];
-  console.log(guessFood);
-}
-
-randomFood();
+exports.game = {
+  wordBank : ["heart and soul", "relax", "burning up", "manic monday", "into the groove", "invisible touch", "rebel yell"]
+};
--- letter.js
+++ letter.js
@@ -1,1 +1,16 @@
 // control whether or not a letter appears as a "_" or as itself on-screen
+
+var Letter = function(let) {
+  
+  this.charac = let;
+  
+  this.appear = false;
+
+  this.letterRender = function() {
+    //if appear is false then show the _
+    //else appear is true then show character
+    return !(this.appear) ? " _ " : this.charac;
+  };
+};
+
+module.exports = Letter;
